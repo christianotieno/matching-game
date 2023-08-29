43ce110b13d277782c140e2b4e7949915a492bd3
Christian Otieno <christian.otieno@saltpay.co>
2023-08-29 07:33:55 +0100

    feat: add game interaction buttons for start end pause and finish

--- src/main.js
+++ src/main.js
@@ -36,6 +36,21 @@ class PlayScene {
         this.scoreText.position.set(10, 10);
         this.app.stage.addChild(this.scoreText);
 
+        this.score = 0;
+        this.isPaused = true; 
+        this.gameOver = false;
+        this.playTime = 0; 
+
+        this.startButton = this.createButton("Start", 10, 50);
+        this.pauseButton = this.createButton("Pause", 10, 100);
+        this.endButton = this.createButton("End", 10, 150);
+        this.restartButton = this.createButton("Restart", 10, 200);
+
+        this.timerText = new PIXI.Text(`Time: ${this.playTime}`, { fontSize: 24, fill: 0xFFFFFF });
+        this.timerText.anchor.set(1, 0); 
+        this.timerText.position.set(this.app.screen.width - 10, 10); 
+        this.app.stage.addChild(this.timerText);
+
         this.gameContainer = new PIXI.Container();
         this.tileGrid = Array.from({ length: this.gridHeight }, () => []);
 
@@ -47,6 +62,11 @@ class PlayScene {
         this.gameContainer.on('click', this.onClick.bind(this));
     }
 
+    updateTimer() {
+        this.playTime++;
+        this.timerText.text = `Time: ${this.playTime}`;
+    }
+
     createTile(color, borderColor = 0x000000) {
         const tile = new PIXI.Graphics();
         tile.lineStyle(2, borderColor);
@@ -56,15 +76,90 @@ class PlayScene {
         return tile;
     }
 
+    createButton(label, x, y) {
+        const button = new PIXI.Text(label, {fontSize: 24, fill: 0xFFFFFF});
+        button.position.set(x,y);
+        button.interactive = true;
+        button.buttonMode = true;
+        button.on("pointerdown", () => this.handleButtonClick(label));
+        this.app.stage.addChild(button);
+        return button;
+    }
+
+    handleButtonClick(label) {
+        switch (label) {
+            case "Start":
+                this.startGame();
+                break;
+            case "Pause":
+                this.togglePause();
+                break;
+            case "Restart":
+                this.restartGame();
+                break;
+            case "End":
+                this.endGame();
+                break;
+        }
+    }
+
+    startGame() {
+        if (!this.isPaused || this.gameOver) return;
+
+        this.isPaused = false;
+        this.startButton.visible = false;
+        this.restartButton.visible = false;
+        this.pauseButton.visible = true; 
+        this.endButton.visible = true; 
+
+        this.playTime = 0;
+        this.timerInterval = setInterval(() => this.updateTimer(), 1000); 
+
+        this.clearTileGrid();
+        this.addInitialRows();
+    }
+
+    togglePause() {
+        this.isPaused = !this.isPaused;
+    }
+
+    restartGame() {
+        this.isPaused = false; 
+        this.gameOver = false; 
+        this.playTime = 0; 
+
+        this.restartButton.visible = false;
+
+        this.timerText.text = `Time: ${this.playTime}`;
+
+        this.clearTileGrid();
+        this.addInitialRows();
+    }
+
+    endGame() {
+        this.gameOver = true;
+        this.isPaused = true;
+        
+  
+        this.pauseButton.visible = false;
+        this.endButton.visible = false;
+
+        this.restartButton.visible = true;
+
+        clearInterval(this.timerInterval);
+
+        const endGameText = new PIXI.Text("Game Over", { fontSize: 36, fill: 0xFF0000 });
+        endGameText.anchor.set(0.5);
+        endGameText.position.set(this.app.screen.width / 2, this.app.screen.height / 2);
+        this.app.stage.addChild(endGameText);
+    }
+
     addInitialRows() {
         const startRow = Math.floor(this.gridHeight / 2);
         for (let row = startRow; row < this.gridHeight; row++) {
             this.addRandomRow(row);
         }
     }
-
-
-
 
     addRandomRow(row) {
         const newRow = [];
@@ -92,7 +187,6 @@ class PlayScene {
             }
         }
     }
-
 
     clearColumns() {
         const tilesToFall = [];
@@ -151,7 +245,6 @@ class PlayScene {
 
         this.app.ticker.add(animation);
     }
-
 
     removeAdjacentTiles(row, col, targetColor) {
         if (row < 0 || row >= this.gridHeight || col < 0 || col >= this.gridWidth || this.tileGrid[row][col].color !== targetColor) {
